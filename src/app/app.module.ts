d0ee3fbad72fa714b86656748598491f1fe7bf3d
Purnima-1 <baranwal.purnimagmail.com>
2022-02-16 17:02:44 -0800

    Homepage component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchedShowsComponent } from './searched-shows/searched-shows.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchedShowsComponent
+    SearchedShowsComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
